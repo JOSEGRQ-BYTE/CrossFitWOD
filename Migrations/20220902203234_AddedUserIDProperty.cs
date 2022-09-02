96126dd0f7d1c3d97e0cd488e02a1ad5dd86fa20
joseramirez <joseramirezq2@gmail.com>
2022-09-02 15:29:47 -0600

    Refactored

---
+++ Migrations/20220902203234_AddedUserIDProperty.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CrossFitWOD.Migrations
+{
+    public partial class AddedUserIDProperty : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "WODs",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "WODs");
+        }
+    }
+}
---
+++ Repositories/WODRepository.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using CrossFitWOD.Data;
+using CrossFitWOD.Interfaces;
+using CrossFitWOD.Models;
+using k8s.KubeConfigModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Org.BouncyCastle.Asn1.X509;
+
+namespace CrossFitWOD.Repositories
+{
+
+    public class WODRepository : GenericRepository<WorkoutOfTheDay>, IWODRepository
+    {
+        public WODRepository(AppDBContext context) : base(context)
+        {
+
+        }
+        public async Task<IEnumerable<WorkoutOfTheDay>> GetWODByUserID(string id)
+        {
+            return await _context.Set<WorkoutOfTheDay>().Where(w => w.UserId==id).ToListAsync();
+        }
+    }
+
+    //public class WODRepository : GenericRepository<WorkoutOfTheDay>, IWODRepository
+    //{
+    /*public WODRepository(AppDBContext context) : base(context)
+    {
+
+    }
+    public async Task<IEnumerable<WorkoutOfTheDay>> GetWODByUserID(string id)
+    {
+        return await _Context.WODs.Where(w => w.UserId==id).ToListAsync();
+    }*/
+
+    // From IWODRepository
+    /*public Task Add(WorkoutOfTheDay model)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Delete(WorkoutOfTheDay model)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<WorkoutOfTheDay> Get(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<WorkoutOfTheDay>> GetAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Save()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update(Guid id, WorkoutOfTheDay model)
+    {
+        throw new NotImplementedException();
+    }
+
+    // From IDisposable
+    public void Dispose()
+    {
+        throw new NotImplementedException();
+    }*/
+    //}
+}
+
